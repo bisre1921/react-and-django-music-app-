c2b3b8db3a8a9eed228c209d7849ea662284b8a4
bisre1921 <bisrattewodros3@gmail.com>
2024-12-18 16:12:11 +0300

    add material UI components

--- music_contoller/api/urls.py
+++ music_contoller/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('' , views.RoomView.as_view())
+    path('' , views.RoomView.as_view()),
+    path('create-room', views.CreateRoomView.as_view())
 ]
